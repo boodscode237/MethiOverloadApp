df06cf1f9e605ba3b78ce68b4840b466f3ee58ad
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-02-03 12:17:50 +0500

    Add project files.

---
+++ MethidOverload/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethidOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new PersonModel("brice", "donny");
+            person.GenerateEmail("hotmail", false);
+            Console.WriteLine(person.Email);
+            Console.ReadLine();
+        }
+    }
+
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public PersonModel()
+        {
+            
+        }
+
+        public PersonModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public PersonModel(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public void GenerateEmail()
+        {
+            GenerateEmail("gmail.net", false);
+        }
+
+        public void GenerateEmail(string domain)
+        {
+            GenerateEmail(domain, false);
+        }
+
+        public void GenerateEmail(bool firstInitialMethod)
+        {
+            GenerateEmail("gmail.net", false);
+        }
+
+        public void GenerateEmail(string domain, bool firstInitialMethod)
+        {
+            if (firstInitialMethod == true)
+            {
+                Email = $"{FirstName.Substring(0, 1)}_{LastName}@{domain}.com";
+            }
+            else
+            {
+                Email = $"{FirstName}-{LastName}@{domain}.com";
+            }
+        }
+
+    }
+
+}
